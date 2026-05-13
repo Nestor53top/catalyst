b6bbefec463cb2be6f8c66ef190ecc63a3a637a5
Nestor53top <varlamovnestor90@gmail.com>
2026-05-13 08:26:56 +0400

    Update csgoinput.cpp

--- catalyst/project/utilities/input/csgoinput.cpp
+++ catalyst/project/utilities/input/csgoinput.cpp
@@ -1,38 +1,38 @@
-#include <stdafx.hpp>
+ #include <stdafx.hpp>
 
 void csgoinput::set_silent_view_angles( const math::vector3& angles ) const
 {
-    const auto last_frame = this->get_last_frame( );
-    if ( !last_frame ) return;
-    g::memory.write<float>( last_frame + 0x10, angles.x );
-    g::memory.write<float>( last_frame + 0x14, angles.y );
-    g::memory.write<float>( last_frame + 0x18, angles.z );
+    auto last = get_last_frame( );
+    if ( !last ) return;
+    g::memory.write<float>( last + 0x10, angles.x );
+    g::memory.write<float>( last + 0x14, angles.y );
+    g::memory.write<float>( last + 0x18, angles.z );
 }
 
 int csgoinput::get_frame_count( ) const
 {
-    const auto input_ptr = g::memory.read<std::uintptr_t>( g::offsets.csgo_input );
-    return input_ptr ? g::memory.read<int>( input_ptr + 0xBF0 ) : 0;
+    auto ptr = g::memory.read<std::uintptr_t>( g::offsets.csgo_input );
+    return ptr ? g::memory.read<int>( ptr + 0xBF0 ) : 0;
 }
 
 std::uintptr_t csgoinput::get_frame_history( ) const
 {
-    const auto input_ptr = g::memory.read<std::uintptr_t>( g::offsets.csgo_input );
-    return input_ptr ? g::memory.read<std::uintptr_t>( input_ptr + 0xBF8 ) : 0;
+    auto ptr = g::memory.read<std::uintptr_t>( g::offsets.csgo_input );
+    return ptr ? g::memory.read<std::uintptr_t>( ptr + 0xBF8 ) : 0;
 }
 
 std::uintptr_t csgoinput::get_last_frame( ) const
 {
-    const auto frame_count = this->get_frame_count( );
-    if ( frame_count <= 0 ) return 0;
-    const auto frame_history_ptr = this->get_frame_history( );
-    return frame_history_ptr ? frame_history_ptr + 96 * ( static_cast< std::uintptr_t >( frame_count ) - 1 ) : 0;
+    int cnt = get_frame_count( );
+    if ( cnt <= 0 ) return 0;
+    auto hist = get_frame_history( );
+    return hist ? hist + 96 * ( cnt - 1 ) : 0;
 }
 
 std::uintptr_t csgoinput::get_frame( int index ) const
 {
-    const auto frame_count = this->get_frame_count( );
-    if ( index < 0 || index >= frame_count ) return 0;
-    const auto frame_history_ptr = this->get_frame_history( );
-    return frame_history_ptr ? frame_history_ptr + 96 * static_cast< std::uintptr_t >( index ) : 0;
+    int cnt = get_frame_count( );
+    if ( index < 0 || index >= cnt ) return 0;
+    auto hist = get_frame_history( );
+    return hist ? hist + 96 * index : 0;
 }

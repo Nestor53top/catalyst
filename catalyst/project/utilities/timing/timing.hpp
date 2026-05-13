4a47ab58b8a07218857dc781cb4d6f9a7d833425
Nestor53top <varlamovnestor90@gmail.com>
2026-05-13 07:28:07 +0400

    Update timing.hpp

--- catalyst/project/utilities/timing/timing.hpp
+++ catalyst/project/utilities/timing/timing.hpp
@@ -2,30 +2,29 @@
 
 namespace timing {
 
-	class limiter
-	{
-	public:
-		explicit limiter( const std::uint32_t target_tps );
-		void limit( ) noexcept;
-		void set_target( std::uint32_t target_tps ) noexcept;
+    class limiter
+    {
+    public:
+        explicit limiter( std::uint32_t target_tps );
+        void limit( ) noexcept;
+        void set_target( std::uint32_t target_tps ) noexcept;
 
-	private:
-		LARGE_INTEGER m_start_time{};
-		std::int64_t m_target_frame_duration{};
-		std::int64_t m_qpc_frequency{};
-	};
+    private:
+        LARGE_INTEGER m_start_time{};
+        std::int64_t m_target_frame_duration{};
+        std::int64_t m_qpc_frequency{};
+    };
 
-	class stopwatch
-	{
-	public:
-		static void start( ) noexcept;
-		static void stop( ) noexcept;
+    class stopwatch
+    {
+    public:
+        static void start( ) noexcept;
+        static void stop( ) noexcept;
+        [[nodiscard]] static float elapsed_duration( ) noexcept;
 
-		[[nodiscard]] static float elapsed_duration( ) noexcept;
+    private:
+        static thread_local std::chrono::steady_clock::time_point m_start;
+        static thread_local std::atomic<float> m_milliseconds;
+    };
 
-	private:
-		static thread_local std::chrono::steady_clock::time_point m_start;
-		static thread_local std::atomic<float> m_milliseconds;
-	};
-
-} // namespace timing
+}
